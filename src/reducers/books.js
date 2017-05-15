611b9e34d93f3f1e8cf6df5a5cd732fd8fff3bfc
Jade Bautista <edaj.bautista@gmail.com>
2017-05-15 17:55:24 +0800

    Initial commit

---
+++ src/reducers/books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        { title: 'One Piece', id: '1' },
+        { title: 'Naruto', id: '2' },
+        { title: 'Code Geass', id: '3' },
+        { title: 'Toriko', id: '4' },
+        { title: 'Hunter X Hunter', id: '5' }
+    ]
+}
