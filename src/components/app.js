3b2c632c0a545e462d0116ce61e35c681779a123
Jade Bautista <edaj.bautista@gmail.com>
2017-05-16 17:35:50 +0800

    Add material ui

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import BookList from '../containers/book-list';
 import BookDetail from '../containers/book-detail'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <BookList />
-        <BookDetail />
-      </div>
+      <MuiThemeProvider>
+        <div style={{width: "100%"}}>
+          <BookList />
+          <BookDetail />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -2,27 +2,45 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { selectBook } from '../actions/index';
 import { bindActionCreators } from 'redux';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 
 class BookList extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {open: false};
+    }
+
+
+    handleToggle = () => this.setState({open: !this.state.open});
+
     renderList() {
+        
         return this.props.books.map((book) => {
             return (
-                <li 
+                <MenuItem 
                     onClick = { () => this.props.selectBook(book) }
                     key = { book.id } 
                     className = "list-group-item"> 
                     { book.title } 
-                </li>
+                </MenuItem>
             )
         })
     }
 
     render() {
         return (
-            <ul className = "list-group col-sm-4">
-                { this.renderList() }
-            </ul>
+            <AppBar
+                onLeftIconButtonTouchTap={this.handleToggle.bind(this)} 
+                title="My App">
+                <Drawer 
+                    
+                    open={this.state.open}>
+                    { this.renderList() }
+                </Drawer>
+            </AppBar>
         )
     }
 }
